bdf707ce41834252841b9670f433bfe804daed9a
Subhalaxmip <115062775+Subhalaxmip@users.noreply.github.com>
2022-11-07 00:35:20 +0530

    ASSIGNMENT 6

---
+++ Assignment6/Delegates/Delegates/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Delegates
+{
+    internal class Program
+    {
+        static int Addition(int a, int b)
+        {
+            return a + b;
+        }
+
+        static void Main(string[] args)
+        {
+
+            operation obj = new operation(Program.Addition);
+
+
+            Console.WriteLine("Addition is={0}", obj(23, 27));
+            Console.ReadLine();
+        }
+    }
+}
